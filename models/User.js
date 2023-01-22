27c1203df30e66073430f16b2eecd74df5b418ad
Diego Mc <projkd1@gmail.com>
2023-01-22 15:53:37 +0200

    build

--- models/User.js
+++ models/User.js
@@ -35,8 +35,7 @@ const UserSchema = new mongoose.Schema(
     },
     imgUrl: {
       type: String,
-      default:
-        'https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png',
+      default: '/default-user-img.png',
     },
     coverUrl: {
       type: String,
